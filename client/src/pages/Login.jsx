3c97265916fa60d9ee6a5774e799abcefecc0aa0
745590 <abhishekchhatraliya@gmail.com>
2025-09-02 07:40:25 +0530

    ADD some importent features

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -67,6 +67,7 @@ const Login = ({ setToken }) => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <Navbar />
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="text-center">
           <svg
