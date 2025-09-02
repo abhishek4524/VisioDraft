adfa0fcb2b282f069e2e18331ebc865ffee6d74d
745590 <abhishekchhatraliya@gmail.com>
2025-09-02 06:35:28 +0530

    ADD some importent features

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -3,7 +3,7 @@ import add from "./add.png"
 import userList from "./group.png"
 import pyqList from "./clipboard.png"
 import pyq from "./pyq.png"
-import community from "./Community-Icon.jpg"
+import community from "./Community-icon.jpg"
 
 export const assets = {
     logo,
